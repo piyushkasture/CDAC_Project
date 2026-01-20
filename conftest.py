a639f4729d60b873e1d9337c5c3963afb8376fc1
Piyush Kasture <piyushkasture120@gmail.com>
2026-01-20 22:36:55 +0530

    upload

---
+++ conftest.py
@@ -0,0 +1,35 @@
+import yaml
+import os
+import pytest
+from utils.DriverManager import DriverManager
+
+
+@pytest.fixture(scope="function")
+def setup_and_teardown(request):
+    config_path = os.path.join("config", "environments", "dev.yaml")
+
+    with open(config_path) as file:
+        config = yaml.safe_load(file)
+
+    page = DriverManager.get_page(
+        browser_name=config.get("browser"),
+        headless=False
+    )
+
+    page.goto(config.get("url"))
+    request.cls.page = page
+
+    yield
+
+    if request.node.rep_call.failed:
+        screenshot_path = f"reports/screenshots/{request.node.name}.png"
+        os.makedirs(os.path.dirname(screenshot_path), exist_ok=True)
+        page.screenshot(path=screenshot_path)
+
+    DriverManager.quit_browser()
+
+
+def pytest_runtest_makereport(item, call):
+    if call.when == "call":
+        item.rep_call = call
+
